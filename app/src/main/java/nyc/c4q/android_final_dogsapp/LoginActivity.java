69e7dfbd247172ba45621963942dfb8fa21884da
Bobby Bah <boubacarbah@ac.c4q.nyc>
2018-02-25 11:20:36 -0500

    first commit

---
+++ app/src/main/java/nyc/c4q/android_final_dogsapp/LoginActivity.java
@@ -0,0 +1,13 @@
+package nyc.c4q.android_final_dogsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
